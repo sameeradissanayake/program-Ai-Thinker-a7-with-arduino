cb218ff3ea96e8def76d4bd72c5338b7beb5701f
sameeradissanayake <samees3dissanayake@gmail.com>
2017-05-11 17:37:06 +0530

    Add files via upload

---
+++ program_Ai_Thinker_a7_with_arduino.ino
@@ -0,0 +1,27 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial1.begin(115200);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial1.available() > 0){
+    while(Serial1.available() > 0){
+      Serial.print((char)Serial1.read());
+    }
+  }
+  if(Serial.available() > 0){
+    while(Serial.available() > 0){
+      //Serial1.print((char)Serial.read());
+      char x = Serial.read();
+      if (x == '*'){
+        Serial1.print((char)26);
+      }
+      else{
+        Serial1.print(x);
+      }
+      
+    }
+  }
+}
